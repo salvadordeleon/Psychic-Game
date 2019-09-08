2fe99c0618912acc9efca196aaf19b1162ce9705
Salvador DeLeon <salvadorvdeleon@gmail.com>
2019-09-08 14:42:07 -0600

    added some JS functionality

---
+++ assets/javascript/game.js
@@ -0,0 +1,13 @@
+var userNumber = 10; // secret number
+
+//generate the random number function
+var randomNumber = math.floor(Math.random() * userNumeber + 1;)
+// ask user to pick number between one and ten
+var attempts = prompt ("I'm thinking of a number between 1 and " + userNumber);
+
+if (myNum === number) {
+
+alert("Nice! You guessed the secret number!")
+
+
+}
